140974f878a138b96b83eae589eb211fc619fcd1
Victor Vilela <vilela_victor@hotmail.com>
2018-02-26 23:20:04 -0500

    asynchronous bat process

--- timer-patterns/asynchronous-execution.js
+++ timer-patterns/asynchronous-execution.js
@@ -8,15 +8,33 @@ function getArrayWithData(){
     return arr;
 }
 
+// function processArray(){
+//     var str = '';
+    
+//     var arr = getArrayWithData();
+//     for (let index = 0; index < arr.length; index++) {
+//         str += arr[index].toString() + ';';        
+//     }
+
+//     console.log(str);
+// }
+
 function processArray(){
     var str = '';
-    
     var arr = getArrayWithData();
-    for (let index = 0; index < arr.length; index++) {
-        str += arr[index].toString() + ';';        
-    }
+    var index = 0;
 
-    console.log(str);
+    setTimeout(() => {
+        for (let init = new Date();new Date() - init< 10; index++) {
+            str += arr[index].toString() + ';'; 
+        }
+        console.log(str);
+        if(index < arr.length){
+            setTimeout(() => {
+                arguments.callee;
+            }, 50);
+        }
+    }, 50);
 }
 
 processArray();
