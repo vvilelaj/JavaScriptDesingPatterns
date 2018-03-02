e7a51c4dcf3f0b04d54a043ee210fdeebb30b9cb
Victor Vilela <vilela_victor@hotmail.com>
2018-03-02 00:08:35 -0500

    a implementarion of recursive settimeout pattern

---
+++ timer-patterns/recursive-settimeout-pattern/recursive-settimeout-pattern/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace recursive_settimeout_pattern.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult GetData(int index)
+        {
+            System.Threading.Thread.Sleep(new Random().Next(5000));
+            return Json(new {
+                Name = "Request " + index.ToString() + " - " + DateTime.Now.ToLongDateString()
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult SetTimeOut()
+        {
+            return View();
+        }
+    }
+}
---
+++ timer-patterns/recursive-settimeout-pattern/recursive-settimeout-pattern/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="recursive_settimeout_pattern.MvcApplication" Language="C#" %>
